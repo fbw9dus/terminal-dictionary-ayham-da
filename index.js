6776fdec74ebb6258ee6acfc53ce6caec34d9f5c
ayham <ayham.alaaeddin@gmail.com>
2020-02-18 09:43:40 +0100

    index Edit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const args = process.argv.slice(2)
 const word = args[0]
 
-
 const https = require('https')
 const axios = require('axios')
 const url = require('url')
@@ -23,18 +22,18 @@ async function getOxFord() {
     const res = await axios.get(request_url, {headers: {"app_id":app_id, "app_key": api_key}})
     const data = await res.data
 
-    const type = data.results[0].lexicalEntries[0].lexicalCategory.text
+    const lexicalCategoryText = data.results[0].lexicalEntries[0].lexicalCategory.text
     const discription = data.results[0].lexicalEntries[0].entries[0].senses[0].shortDefinitions
     const definitions1 = data.results[0].lexicalEntries[0].entries[0].senses[1].definitions
     const definitions2 = data.results[0].lexicalEntries[0].entries[0].senses[2].definitions
-
     const Provided = data.metadata.provider
 
-    console.log(`${word} (${type}) 
+    console.log(`
+    \n *  ${word} (${lexicalCategoryText}) 
     \n 1. ${discription}
     \n 2. ${definitions1}
     \n 3. ${definitions2}
-    \n Provided by:${Provided} `)
+    \n Provided by:${Provided}`)
 }
 
 getOxFord()
